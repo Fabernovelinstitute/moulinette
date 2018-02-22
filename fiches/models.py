b44535dfa9118589006c0bf5a6a1d1c6e150b263
LucasBerbesson <lucasberbesson@macbook-pro-de-lucas.home>
2018-02-22 15:19:28 +0100

    amelioration des models liés aux fiches et aux clients

--- fiches/models.py
+++ fiches/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
+    logo = models.ImageField()
 
     def __str__(self):
         return self.name
@@ -19,11 +20,15 @@ class Fiche(models.Model):
     project_title = models.CharField(max_length=300)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     format = models.ForeignKey(Format, on_delete=models.CASCADE,null=True,blank=True)
+    budget = models.IntegerField()
     start_date = models.DateField(null=True,blank=True)
     end_date = models.DateField(null=True,blank=True)
     image = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True, help_text="WYSIWYG")
+    custom_ref = models.URLField(null=True,blank=True)
+
 
     def __str__(self):
-        return "{} - {}".format(self.client,self.project_title)
+        return "{} - {}".format(self.client, self.project_title)
 
 
