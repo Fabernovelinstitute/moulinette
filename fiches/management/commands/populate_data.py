cd66e6225715c8638a842d5a2056d0d323430a96
LucasBerbesson <lucasberbesson@macbook-pro-de-lucas.home>
2018-02-22 15:21:58 +0100

    different formats enregistrés en dur dans une tache de management

--- fiches/management/commands/populate_data.py
+++ fiches/management/commands/populate_data.py
@@ -3,7 +3,13 @@ import csv
 import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from fiches.models import Client, Fiche
+from fiches.models import Client, Fiche, Format
+
+formats = ["Keynote, veille et analyse", "Communauté", "Communication interne", "Digital Academy",
+           "Digital Coaching", "E-learning", "Formation mindset", "Formation skills", "Hackathon",
+           "Learning Expedition", "Programme intrapreneuriat", "Reverse Mentoring",
+           "Séminaires métier",
+           "Séminaire pour dirigeants", "Synergies internes", "Accompagnement stratégique"]
 
 
 class Command(BaseCommand):
@@ -23,6 +29,12 @@ class Command(BaseCommand):
                 fiche, fcreated = Fiche.objects.get_or_create(project_title=fiche[1], client=client)
                 if fcreated:
                     fiche_counter += 1
+        format_counter = 0
+        for format in formats:
+            new_format, created = Format.objects.get_or_create(name=format)
+            if created:
+                format_counter += 1
 
         print("{} client créés".format(client_counter))
         print("{} fiches créés".format(fiche_counter))
+        print("{} formats créés".format(format_counter))
